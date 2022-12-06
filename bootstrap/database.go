90fa214d0817827749a9e9c7b070a4d3d9730ebf
ningkang <ningkang@smartmore.com>
2022-12-06 11:43:23 +0800

    手机号码是否注册

--- bootstrap/database.go
+++ bootstrap/database.go
@@ -1,6 +1,7 @@
 package bootstrap
 
 import (
+	"Rhub/app/models/user"
 	"Rhub/pkg/config"
 	"Rhub/pkg/database"
 	"errors"
@@ -47,4 +48,5 @@ func SetupDB() {
 	database.SQLDB.SetMaxIdleConns(config.GetInt("database.mysql.max_idle_connections"))
 	// 设置每个链接的过期时间
 	database.SQLDB.SetConnMaxLifetime(time.Duration(config.GetInt("database.mysql.max_life_seconds")) * time.Second)
+	database.DB.AutoMigrate(&user.User{})
 }
