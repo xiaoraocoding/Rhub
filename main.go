396c99ca13547c8c99e7cecc6df3464be7dae481
ningkang <ningkang@smartmore.com>
2022-12-06 10:35:25 +0800

    引入gin框架

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("this is test my env")
+	// 初始化 Gin 实例
+	r := gin.Default()
+
+	// 注册一个路由
+	r.GET("/", func(c *gin.Context) {
+
+		// 以 JSON 格式响应
+		c.JSON(http.StatusOK, gin.H{
+			"Hello": "World!",
+		})
+	})
+
+	// 运行服务
+	r.Run()
 }
